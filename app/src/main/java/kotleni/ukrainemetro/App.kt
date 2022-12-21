7a1c78336f5d05dccdab8fde77ff863b435c25d4
kotleni <yavarenikya@gmail.com>
2022-12-21 11:03:31 +0200

    Enabled dynamic colors (material 3)

--- app/src/main/java/kotleni/ukrainemetro/App.kt
+++ app/src/main/java/kotleni/ukrainemetro/App.kt
@@ -1,5 +1,13 @@
 package kotleni.ukrainemetro
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 
-class App: Application() { }
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Enable dynamic colors (material 3)
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}

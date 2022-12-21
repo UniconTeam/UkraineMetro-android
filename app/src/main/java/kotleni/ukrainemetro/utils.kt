0b8869c08db979218106289297cbdc1c3aca82f3
kotleni <yavarenikya@gmail.com>
2022-12-21 19:37:09 +0200

    Fixed: bubble twitching on scroll Just removed toInt and toFloat converting.

--- app/src/main/java/kotleni/ukrainemetro/utils.kt
+++ app/src/main/java/kotleni/ukrainemetro/utils.kt
@@ -3,6 +3,7 @@ package kotleni.ukrainemetro
 import android.content.Context
 import android.graphics.Paint
 import android.graphics.Rect
+import android.graphics.RectF
 import android.text.TextPaint
 import android.util.TypedValue
 import androidx.annotation.AttrRes
@@ -25,14 +26,14 @@ fun getTextBackgroundSize(
     y: Float,
     text: String,
     paint: TextPaint
-): Rect {
+): RectF {
     val fontMetrics: Paint.FontMetrics = paint.fontMetrics
     val halfTextLength = paint.measureText(text) / 2 + 5
-    return Rect(
-        (x - halfTextLength).toInt(),
-        (y + fontMetrics.top).toInt(),
-        (x + halfTextLength).toInt(),
-        (y + fontMetrics.bottom).toInt()
+    return RectF(
+        (x - halfTextLength),
+        (y + fontMetrics.top),
+        (x + halfTextLength),
+        (y + fontMetrics.bottom)
     )
 }
 

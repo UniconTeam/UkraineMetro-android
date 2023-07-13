8cfe9433e22fa6d22f823babb709239d4360b3d4
kotleni <yavarenikya@gmail.com>
2023-07-13 11:35:38 +0300

    added hilt

--- app/src/main/java/uniconteam/ukrainemetro/App.kt
+++ app/src/main/java/uniconteam/ukrainemetro/App.kt
@@ -2,7 +2,9 @@ package uniconteam.ukrainemetro
 
 import android.app.Application
 import com.google.android.material.color.DynamicColors
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class App: Application() {
     override fun onCreate() {
         super.onCreate()

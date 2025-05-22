fe31ec84fa8dd96f3bedf129cf6570a8527971ad
Viktor Varenik <yavarenikya@gmail.com>
2025-05-22 13:11:22 +0300

    feat: impl basic logic for viewmodel

--- composeApp/src/commonMain/kotlin/team/unicon/ukrainemetro/ui/main/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/team/unicon/ukrainemetro/ui/main/MainScreen.kt
@@ -1,15 +1,21 @@
 package team.unicon.ukrainemetro.ui.main
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.jetbrains.compose.resources.painterResource
 import team.unicon.ukrainemetro.LocalStrings
 import ukrainemetro.composeapp.generated.resources.Res
@@ -18,7 +24,12 @@ import ukrainemetro.composeapp.generated.resources.compose_multiplatform
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     val strings = LocalStrings.current
+
+    LaunchedEffect(Unit)  {
+        viewModel.loadSubway()
+    }
 
     Scaffold(
         topBar = {
@@ -27,17 +38,34 @@ fun MainScreen(viewModel: MainViewModel) {
             )
         }
     ) { paddingValues ->
-        Column(
-            modifier = Modifier.padding(paddingValues)
-                .padding(16.dp)
-        ) {
-            Image(
-                painterResource(Res.drawable.compose_multiplatform),
-                null
-            )
-            Text(
-                text = strings.cityKharkiv.resolve()
-            )
+        when(uiState) {
+            is MainViewModel.UIState.Loading -> {
+                Column(
+                    modifier = Modifier.padding(paddingValues)
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
+
+            is MainViewModel.UIState.Present -> {
+                val presentUiState = (uiState as MainViewModel.UIState.Present)
+
+                Column(
+                    modifier = Modifier.padding(paddingValues)
+                        .padding(16.dp)
+                ) {
+                    Image(
+                        painterResource(Res.drawable.compose_multiplatform),
+                        null
+                    )
+                    Text(
+                        text = presentUiState.subwayInfo.name.resolve()
+                    )
+                }
+            }
         }
     }
 }
--- composeApp/src/commonMain/kotlin/team/unicon/ukrainemetro/ui/main/MainViewModel.kt
+++ composeApp/src/commonMain/kotlin/team/unicon/ukrainemetro/ui/main/MainViewModel.kt
@@ -1,10 +1,26 @@
 package team.unicon.ukrainemetro.ui.main
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import team.unicon.ukrainemetro.entities.Subway
+import team.unicon.ukrainemetro.entities.SubwayInfo
 import team.unicon.ukrainemetro.repositories.SubwaysRepository
 
 class MainViewModel(
     private val subwaysRepository: SubwaysRepository
 ) : ViewModel() {
+    sealed interface UIState {
+        data object Loading : UIState
+        data class Present(val subwayInfo: SubwayInfo) : UIState
+    }
 
+    private val _uiState: MutableStateFlow<UIState> = MutableStateFlow(UIState.Loading)
+    val uiState: StateFlow<UIState> = _uiState.asStateFlow()
+
+    suspend fun loadSubway() {
+        val subwayInfo = subwaysRepository.getSubwayInfo(Subway.Kharkiv)
+        _uiState.value = UIState.Present(subwayInfo)
+    }
 }

9cbc4642d8e8e5747db7c50a0b48ee7c8f265e02
Viktor Varenik <yavarenikya@gmail.com>
2025-05-23 11:36:02 +0300

    feat: impl di for web

--- composeApp/src/wasmJsMain/kotlin/team/unicon/ukrainemetro/main.kt
+++ composeApp/src/wasmJsMain/kotlin/team/unicon/ukrainemetro/main.kt
@@ -3,9 +3,13 @@ package team.unicon.ukrainemetro
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import team.unicon.ukrainemetro.di.initKoin
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    initKoin {
+
+    }
     ComposeViewport(document.body!!) {
         App()
     }

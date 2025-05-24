2283e08722473be7956d29d89ef21e0b1bb4ec41
Viktor Varenik <yavarenikya@gmail.com>
2025-05-24 18:24:05 +0300

    chore: impl basic settings engine

--- composeApp/src/commonMain/kotlin/team/unicon/ukrainemetro/ui/settings/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/team/unicon/ukrainemetro/ui/settings/SettingsScreen.kt
@@ -2,6 +2,8 @@ package team.unicon.ukrainemetro.ui.settings
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.ArrowBack
@@ -12,11 +14,32 @@ import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import team.unicon.ukrainemetro.ui.settings.subitems.SettingSwitchItem
+
+sealed interface SettingItem {
+    data class Switch(
+        val title: String,
+        val description: String,
+        val key: String,
+        val defaultValue: Boolean
+    ) : SettingItem
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsScreen(viewModel: SettingsViewModel, onNavigateBack: () -> Unit) {
+    val settingItems = remember { listOf(
+        SettingItem.Switch(
+            title = "Alternative branch colors",
+            description = "Use theme provided colors for branches instead of vanilla.",
+            key = "alternative_colors",
+            defaultValue = false
+        )
+    ) }
+
     Scaffold(
         topBar = {
             LargeTopAppBar(
@@ -29,10 +52,22 @@ fun SettingsScreen(viewModel: SettingsViewModel, onNavigateBack: () -> Unit) {
             )
         }
     ) { paddingValues ->
-        Column(
+        LazyColumn(
             modifier = Modifier.padding(paddingValues)
         ) {
-
+            items(settingItems) { item ->
+                when(item) {
+                    is SettingItem.Switch -> {
+                        SettingSwitchItem(
+                            isEnabled = true,
+                            title = item.title,
+                            description = item.description,
+                            value = false,
+                            onChange = { }
+                        )
+                    }
+                }
+            }
         }
     }
 }
---
+++ composeApp/src/commonMain/kotlin/team/unicon/ukrainemetro/ui/settings/subitems/SettingSwitchItem.kt
@@ -0,0 +1,60 @@
+package team.unicon.ukrainemetro.ui.settings.subitems
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SettingSwitchItem(
+    isEnabled: Boolean = true,
+    title: String,
+    description: String,
+    value: Boolean,
+    onChange: (isEnabled: Boolean) -> Unit
+) {
+    Row(
+        modifier = Modifier.clickable(onClick = { if(isEnabled) onChange(!value) })
+    ) {
+        Row(
+            modifier = Modifier.padding(
+                top = 16.dp,
+                bottom = 16.dp,
+                start = 24.dp,
+                end = 24.dp
+            ),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth()
+                    .weight(1f)
+            ) {
+                Text(
+                    modifier = Modifier.alpha(if(isEnabled) 1f else 0.5f),
+                    text = title,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Text(
+                    modifier = Modifier.alpha(if(isEnabled) 1f else 0.5f),
+                    text = if(isEnabled) description else "Feature not available on this device.",
+                    style = MaterialTheme.typography.bodyLarge,
+                )
+            }
+
+            Switch(
+                checked = value,
+                onCheckedChange = onChange,
+                enabled = isEnabled
+            )
+        }
+    }
+}
